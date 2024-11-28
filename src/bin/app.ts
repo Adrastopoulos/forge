f0165a0c0e95b56a80a7a5e33c1fe09ece80c3ef
Gabriel Hall <adrastopoulos@gmail.com>
2024-11-27 22:50:06 -0500

    feat: update app

--- src/bin/app.ts
+++ src/bin/app.ts
@@ -1,11 +1,29 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
-import { CiForgeStack } from "../lib/ciForgeStack";
+import { Forge as ForgeStack } from "../lib/forge";
+
+import { SonarQubeStack } from "../lib/sonarqube";
+import { JenkinsStack } from "../lib/jenkins";
 
 const app = new cdk.App();
-new CiForgeStack(app, "CiForgeStack", {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
-  },
+
+const env = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION,
+}
+
+const forgeStack = new ForgeStack(app, "ForgeStack", {
+  env
 });
+
+const sonarqube = new SonarQubeStack(app, "SonarQube", {
+  env,
+  vpc: forgeStack.vpc,
+});
+
+const jenkins = new JenkinsStack(app, "Jenkins", {
+  env,
+  vpc: forgeStack.vpc,
+  sonarQubeUrl: sonarqube.loadBalancer.loadBalancerDnsName,
+});
+
