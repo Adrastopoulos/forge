71e6b494809ca0e8566c4571467f3c37fc407f7b
Gabriel Hall <adrastopoulos@gmail.com>
2024-12-01 04:17:46 -0500

    feat: update eslint setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,33 +1,28 @@
-/** @type {import("eslint").Linter.Config} */
-const config = {
-  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
-  env: {
-    es2022: true,
-    node: true,
-  },
-  parser: '@typescript-eslint/parser',
-  parserOptions: { project: true },
-  plugins: ['@typescript-eslint', 'import'],
-  rules: {
-    'turbo/no-undeclared-env-vars': 'off',
-    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
-    '@typescript-eslint/consistent-type-imports': [
-      'warn',
-      { prefer: 'type-imports', fixStyle: 'separate-type-imports' },
-    ],
-    '@typescript-eslint/no-misused-promises': [2, { checksVoidReturn: false }],
-    'import/consistent-type-specifier-style': ['error', 'prefer-top-level'],
-  },
-  ignorePatterns: [
-    '**/*.config.js',
-    '**/*.config.cjs',
-    '**/.eslintrc.cjs',
-    '**/.next',
-    '**/dist',
-    'pnpm-lock.yaml',
-    'cdk.out/**/*',
-  ],
-  reportUnusedDisableDirectives: true,
-};
+import typescriptPlugin from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+import prettierConfig from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
 
-module.exports = config;
+export default [
+  {
+    plugins: {
+      '@typescript-eslint': typescriptPlugin,
+      import: importPlugin,
+    },
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: { project: true },
+    },
+    files: ['src/**/*.ts'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+      '@typescript-eslint/consistent-type-imports': [
+        'warn',
+        { prefer: 'type-imports', fixStyle: 'separate-type-imports' },
+      ],
+      '@typescript-eslint/no-misused-promises': ['error', { checksVoidReturn: false }],
+      'import/consistent-type-specifier-style': ['error', 'prefer-top-level'],
+    },
+  },
+  prettierConfig,
+];
